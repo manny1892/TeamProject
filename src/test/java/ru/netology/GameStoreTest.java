0e00bd535b50f08764b5eb1aa47d4658f9b7dc86
manny1892 <manny1892@gmail.com>
2022-07-05 16:14:16 +0300

    gamestore tests before merge

--- src/test/java/ru/netology/GameStoreTest.java
+++ src/test/java/ru/netology/GameStoreTest.java
@@ -23,6 +23,26 @@ public class GameStoreTest {
     // другие ваши тесты
 
     @Test
+    public void shouldReturnFalseAddGames() {
+
+        GameStore store = new GameStore();
+        Game game = store.publishGame("Нетология Баттл Онлайн", "Аркады");
+        Game game2 = new Game("Дюна", "Стратегия", store);
+
+        assertFalse(store.containsGame(game2));
+    }
+
+    @Test
+    public void shouldReturnFalseContainsGame() {
+
+        GameStore store = new GameStore();
+
+        Game game2 = new Game("Дюна", "Стратегия", store);
+
+        assertFalse(store.containsGame(game2));
+    }
+
+    @Test
     public void shouldGetMostPlayer() {
 
         GameStore store = new GameStore();
@@ -34,6 +54,42 @@ public class GameStoreTest {
 
         String actual = store.getMostPlayer();
         String expected = "Joker";
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldGetMostPlayerEqualsZero() {
+
+        GameStore store = new GameStore();
+        Game game = store.publishGame("Нетология Баттл Онлайн", "Аркады");
+
+        store.addPlayTime("Alex", 0);
+
+        String actual = store.getMostPlayer();
+        String expected = "Alex";
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldGetMostPlayerReturnNull() {
+
+        GameStore store = new GameStore();
+
+        String actual = store.getMostPlayer();
+        String expected = null;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldReturnNullGetMostPlayerNegativeValue() {
+
+        GameStore store = new GameStore();
+        Game game = store.publishGame("Нетология Баттл Онлайн", "Аркады");
+
+        store.addPlayTime("Alex", -1);
+
+        String actual = store.getMostPlayer();
+        String expected = null;
         assertEquals(expected, actual);
     }
 
@@ -75,5 +131,45 @@ public class GameStoreTest {
         int actual = store.getSumPlayedTime();
         int expected = 7;
         assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldReturnZeroGetSumPlayedTime() {
+
+        GameStore store = new GameStore();
+
+        int actual = store.getSumPlayedTime();
+        int expected = 0;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldGetSumZeroPlayedTime() {
+
+        GameStore store = new GameStore();
+
+        store.addPlayTime("Alex", 0);
+        store.addPlayTime("Joker", 0);
+        store.addPlayTime("Mike", 0);
+
+        int actual = store.getSumPlayedTime();
+        int expected = 0;
+        assertEquals(expected, actual);
+
+    }
+
+    @Test
+    public void shouldGetSumOnePlayedTime() {
+
+        GameStore store = new GameStore();
+
+        store.addPlayTime("Alex", 0);
+        store.addPlayTime("Joker", 1);
+        store.addPlayTime("Mike", 0);
+
+        int actual = store.getSumPlayedTime();
+        int expected = 1;
+        assertEquals(expected, actual);
+
     }
 }
